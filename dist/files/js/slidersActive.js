32c30280c901bd3e9b0285457645ec48e8e0b907
nkoort <maks-g@i.ua>
2022-01-30 23:25:13 +0200

    master

--- dist/files/js/slidersActive.js
+++ dist/files/js/slidersActive.js
@@ -16,7 +16,6 @@
 //     slidesToShow: 3,
 //     slidesToScroll: 3
 //   });
-
 $(document).ready(function(){
     $('.coldEat-slider-items').slick({
         arrows: false,
@@ -24,19 +23,104 @@ $(document).ready(function(){
         slidesToShow: 4,
         slidesToScroll: 1,
         speed: 1000,
-        touchThreshold: 10,
+        touchThreshold: 15,
         waitForAnimate: false,
         centerMode: true,
         variableWidth: true,
+        initialSlide:1,
+    });
+    $('.hootEat-slider-items').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide: 2,
+    });
+    $('.meetEat-slider-items').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide: 3,
+    });
+    $('.soops-slider-items').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide: 4,
+    });
+    $('.fisthEat-slider-items').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide: 3,
+    });
+    $('.bbqEat-slider-items').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide: 1,
+    });
+    $('.chefEat-slider-items').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide: 2,
+    });
+    $('.drinks-slider-items').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide: 3,
     });
     $('.menu-slider').slick({
         arrows: false,
         dots: false,
-        infinite: false,
+        infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
         speed: 1000,
-        touchThreshold: 10,
+        touchThreshold: 15,
         waitForAnimate: false,
         centerMode: false,
         variableWidth: true,
